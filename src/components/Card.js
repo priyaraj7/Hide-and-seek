fade5def4ac15a8957e63150e3e191db98a3c7db
Priyaraj7 <suppichala@gmail.com>
2020-04-21 19:19:52 -0700

    Initial commit.

---
+++ src/components/Card.js
@@ -0,0 +1,61 @@
+import React from "react";
+import giraffe from "../assets/giraffe.jpg";
+
+function Card() {
+  return (
+    <React.Fragment>
+      <div className="card-wrapper">
+        <img src={giraffe} alt="" />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Card;
+
+// import React from "react";
+// import Item from "./Item";
+// import image1 from "../assets/1.jpg";
+// import image2 from "../assets/2.jpg";
+// import image3 from "../assets/3.jpg";
+// import { CardDeck } from "react-bootstrap";
+
+// const masterTicketList = [
+//   {
+//     names: "Giraffe",
+//     location: image1,
+//     issue: "Unique activities we can do together, led by a world of hosts",
+//   },
+//   {
+//     names: "Monthly stays",
+//     location: image2,
+//     issue: "Make Airbnb your home, for stays of a month or longer.",
+//   },
+//   {
+//     names: "Frontline stays",
+//     location: image3,
+//     issue: "Find or provide accomadation for COVID-19 responders.",
+//   },
+// ];
+
+// function ItemList() {
+//   return (
+//     <React.Fragment>
+//       <div padding="10px" margin="10px">
+//         <hr />
+//         <CardDeck>
+//           {masterTicketList.map((ticket, index) => (
+//             <Item
+//               names={ticket.names}
+//               location={ticket.location}
+//               issue={ticket.issue}
+//               key={index}
+//             />
+//           ))}
+//         </CardDeck>
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+
+// export default ItemList;
---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <h1>Lets Play Hide and Seek</h1>;
+}
+
+export default Header;
